7743e6515031500a241e8eb316cf1a4f11c85268
JAAR <joe.aaron.acosta@outlook.com>
2026-07-06 23:29:18 -0700

    Created GitHub Repo and learned to download data from NCBI

---
+++ rna_seq_intro.R
@@ -0,0 +1,16 @@
+# RNA-Seq Introduction Notebooks 
+# Authors: Acosta, Joe
+# Date: 07/06/2026
+
+# Linking github repository
+# usethis::create_from_github(
+#   "https://github.com/JA-Acosta/rna_seq_intro.git",
+#   destdir = "C:/Users/Drago/source/repos"
+# )
+
+# Data set info :
+#   - NCBI GEO Data set found w/accession ID: 
+#   - FPKM Normalized
+#   - Don't forget gunzip
+
+# data set was added to .gitignore
