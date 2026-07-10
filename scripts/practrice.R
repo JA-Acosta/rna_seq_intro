d66935d7368e5187dbf4fade905f01f6ac05a0a2
JAAR <joe.aaron.acosta@outlook.com>
2026-07-09 23:13:30 -0700

    Practiced everything I've learned to date starting from loading the dataset

--- scripts/practrice.R
+++ scripts/practrice.R
@@ -4,20 +4,58 @@
 ## Authors: Acosta, Joe A
 ## Date: 07/09/2026
 
-# Questions Developed over course of practice:
-# 1. What are the different types of joins:
-  # inner joins consist of the data shared by both df only
-  # left join is all data on the left. If there is corresponding data in the
-  # right df, use it
-  # right join is all data on the right df. If there is corresponding data in the
-  # left df, use it
-  # outer join brings in all data from both df
+# Working Directory
+setwd("C:/Users/Drago/source/repos/rna_seq_intro/scripts")
 
-# 2. Why make the data a long dataset
-  # Didn't find a satisfactory answer outside of it being the best practice for
-  # working in R for plots
+# Load Libraries
+library(tidyverse)
+library(GEOquery)
 
+# Load Data
+dat <- read.csv(file = "../data/GSE183947_fpkm.csv")
 
-# 3. Look up gsub
-  # allows for substitution of strings and can even use regex
+dim(dat)
 
+# Load Metadata
+gse <- getGEO(GEO = "GSE183947", GSEMatrix = TRUE) # didn't remember GSEMatrix
+metadata <- pData(phenoData(gse[[1]]))
+
+head(metadata)
+colnames(metadata)
+
+# Create Metadata Subset and Clean It
+metadata.modified <- metadata %>%
+  select(c(1, 10, 11, 17)) %>%
+  rename("tissue" = characteristics_ch1) %>%
+  rename("metastasis" = characteristics_ch1.1) %>%
+  rename("sample" = description) %>%
+  mutate(tissue = gsub("tissue: ", "", tissue)) %>%
+  mutate(metastasis = gsub("metastasis: ", "", metastasis))
+
+head(metadata.modified)
+dim(metadata.modified)
+
+# Create Long Dataset
+dat.long <- dat %>%
+  rename(gene = X) %>%
+  gather(key = "sample", value = "FPKM", -gene) # didn't remember gather
+
+# Merge Metadata and Long Dataset
+dat.long <- dat.long %>%
+  left_join(., metadata.modified, by = c("sample" = "sample"))
+
+head(dat.long)
+dim(dat.long)
+
+# Data Exploration
+dat.long %>%
+  filter(gene == "BRCA1" | gene == "BRCA2") %>% # didn't remember filter
+  group_by(gene, tissue) %>% # didn't remember groupby
+  summarise( # didn't remembered summarise
+    mean_FPKM = mean(FPKM),
+    median_FPKM = median(FPKM)
+  ) %>%
+  arrange(mean_FPKM)
+
+# First Collect the relevant genes then conduct data exploration on them.
+# In this case, we are looking at the BRCA1 and BRCA2 genes 
